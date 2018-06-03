90c41026306ded1733d4f4dac15cff767e1267d6
DESKTOP-TRJ5BE0\David <david@bernhardt-nidderau.de>
2018-06-03 19:11:06 +0200

    Merge Commit

---
+++ Assets/Scripts/BuildinPlacerHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildinPlacerHelper : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.GetComponentInChildren<Building>())
+        {
+            print("test");
+        }
+    }
+}
